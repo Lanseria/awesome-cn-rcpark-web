087a63bf27b167c31f2d4498dcb388e3de778c0d
lanseria <zhangchao564265135@hotmail.com>
2022-04-07 17:58:56 +0800

    chore: init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <main
+    font-sans p="x-4 y-10"
+    text="gray-700 dark:gray-200"
+    bg="gray-100 dark:gray-900"
+    border="1px solid gray-300 dark:gray-800"
+  >
+    <router-view />
+  </main>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div
+    color="red dark:red-500"
+    bg="blue dark:blue-500"
+    border="1px solid red dark:red-500"
+    p="x-4 y-4"
+    rounded
+  >
+    Hello World
+  </div>
+  <div
+    flex
+    justify-center
+    items-center
+    bg="red dark:red-500"
+    sm="hover:bg-red-500"
+    rounded="md hover:md"
+  >
+    <div>Hello</div>
+    <div>World</div>
+    <div>
+      <div>2LineHelloWorld</div>
+      <div>2LineHelloWorld</div>
+    </div>
+  </div>
+  <div
+    btn
+    bg="purple dark:purple-500 hover:purple-500"
+    shadow="md"
+  >
+    按钮
+  </div>
+</template>
